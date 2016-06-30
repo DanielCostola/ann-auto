41886e84b97385973f2727f53cd010e82802f4bb
btb14143 <btb14143@uni.strath.ac.uk>
2016-06-30 15:10:07 +0100

    Add files via upload

---
+++ ann.m
@@ -0,0 +1,7 @@
+x=0:0.01:100;
+y=x.^2;
+net=newff(minmax(x),[24 1], {'logsig','purelin'},'trainlm');
+net.trainparam.epochs=8000;
+net.trainparam.goal=1e-25;
+net.trainparam.goal=0.01;
+net=train(net,x,y);
